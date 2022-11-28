88ae5851ca1b1cab5ea0f9730f825979b1ffa279
gvrDev <guilhermevrodrigues123@gmail.com>
2022-11-28 12:26:03 -0300

    added .env validation and generated hero application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validate } from './env.validation';
+import { HeroModule } from './hero/hero.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ validate: validate, expandVariables:true }), HeroModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/env.validation.ts
@@ -0,0 +1,23 @@
+import { plainToInstance } from "class-transformer";
+import { IsNotEmpty, IsString, validateSync } from "class-validator";
+
+class EnvironmentVariables {
+  @IsNotEmpty()
+  @IsString()
+  DB_CONN: string;
+}
+
+export function validate(config: Record<string, unknown>){
+  const validatedConfig = plainToInstance(
+    EnvironmentVariables,
+    config,
+    { enableImplicitConversion: true }
+  );
+  const errors = validateSync(validatedConfig, { skipMissingProperties: false });
+
+  if(errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
