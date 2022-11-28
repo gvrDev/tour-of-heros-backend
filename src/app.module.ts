75f5444446a037d8e9e776308162aa5e342bfe41
gvrDev <guilhermevrodrigues123@gmail.com>
2022-11-28 12:46:35 -0300

    added mongoose module root on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validate } from './env.validation';
@@ -8,6 +9,7 @@ import { HeroModule } from './hero/hero.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ validate: validate, expandVariables:true }),
+    MongooseModule.forRoot(process.env.DB_CONN),
     HeroModule
   ],
   controllers: [AppController],
